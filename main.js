ac904fdef5d66e7602b6543e38c8f494a6eb356a
MariaClaudia1982 <mariaclaudiapio@gmail.com>
2022-07-23 17:26:43 -0300

    adicionando eventos de teclado ao código

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+    const ELEMENTO = document.querySelector(seletorAudio);
+
+    if (ELEMENTO && ELEMENTO.localName === 'audio') {
+        ELEMENTO.play();
+    } else {
+        alert('Elemento não encontrado ou seletor inválido');
+    }
+
 }
 
 const LISTA_DE_TECLAS = document.querySelectorAll('.tecla');
@@ -11,7 +18,19 @@ for (let contador = 0; contador < LISTA_DE_TECLAS.length; contador++) {
 
     TECLA.onclick = function () {
         tocaSom(ID_AUDIO);
-    };
+    }
+
+    TECLA.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            TECLA.classList.add('ativa');
+        }
+    }
+
+    TECLA.onkeyup = function () {
+        TECLA.classList.remove('ativa');
+    }
 }
+
 
 
